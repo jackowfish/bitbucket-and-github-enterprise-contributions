7fe4431364d23a79a6687e7d4d920914a8ba6c9d
Jack Decker <jadecker@cisco.com>
2024-02-01 16:08:04 -0800

    add code in files types: ,.go,.json,.yaml,.mod,.sum,.tf,.md,.txt remove code in files types: .json,.go,.yaml,.mod,.sum,,.toml,.yml,.md,.sh,.txt

--- content.tf
+++ content.tf
@@ -1,3 +1,4 @@
 resource "random_string" "nndaa" { length = 10 }
 resource "random_string" "rcakd" { length = 10 }
 resource "random_string" "aenhk" { length = 10 }
+resource "random_string" "xtdep" { length = 10 }
