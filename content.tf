5d7df0847b937578ef6ab1c5be9b697b5fd0ab66
Jack Decker <jadecker@cisco.com>
2024-03-06 10:57:22 -0800

    add code in files types: .go,.tf remove code in files types: .go

--- content.tf
+++ content.tf
@@ -3,3 +3,4 @@ resource "random_string" "rcakd" { length = 10 }
 resource "random_string" "aenhk" { length = 10 }
 resource "random_string" "unrsc" { length = 10 }
 resource "random_string" "erqpf" { length = 10 }
+resource "random_string" "effmt" { length = 10 }
