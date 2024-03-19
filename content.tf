eeb4d8cf172b237817e243fb4f48ae704737eb12
Jack Decker <jadecker@cisco.com>
2024-03-19 14:35:23 -0700

    add code in files types: ,.lock,.toml,.sh,.go,.md,.tf remove code in files types: ,.lock,.toml,.sh,.go,.md,.tf

--- content.tf
+++ content.tf
@@ -25,4 +25,4 @@ resource "random_string" "ikjaj" { length = 10 }
 resource "random_string" "eqhnd" { length = 10 }
 resource "random_string" "ppwpo" { length = 10 }
 resource "random_string" "mvvlg" { length = 10 }
-resource "random_string" "chjdu" { length = 10 }
+resource "random_string" "aljkk" { length = 10 }
