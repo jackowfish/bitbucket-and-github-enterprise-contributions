16b1744f2953e2f2140a72ee841dfa9757cbe8d8
Jack Decker <jadecker@cisco.com>
2024-04-08 16:56:08 -0700

    add code in files types: ,.tf remove code in files types: ,.tf

--- content.tf
+++ content.tf
@@ -5,4 +5,4 @@ resource "random_string" "unrsc" { length = 10 }
 resource "random_string" "erqpf" { length = 10 }
 resource "random_string" "waokm" { length = 10 }
 resource "random_string" "vmmlr" { length = 10 }
-resource "random_string" "uxduy" { length = 10 }
+resource "random_string" "skoow" { length = 10 }
