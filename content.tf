7b2c3befd37f91e0ca4f13e6b7db7b1dbb352a2d
Jack Decker <jadecker@cisco.com>
2024-08-21 13:43:16 -0700

    add code in files types: .tf remove code in files types:

--- content.tf
+++ content.tf
@@ -11,3 +11,4 @@ resource "random_string" "ubiim" { length = 10 }
 resource "random_string" "tpbmf" { length = 10 }
 resource "random_string" "xcmxd" { length = 10 }
 resource "random_string" "cjddh" { length = 10 }
+resource "random_string" "klfoh" { length = 10 }
