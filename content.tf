3e9fbe1d8bd8a575b43e71cd3d8612fce2f9cdb4
Jack Decker <jadecker@cisco.com>
2024-08-27 13:09:06 -0700

    add code in files types: .hcl remove code in files types:

--- content.tf
+++ content.tf
@@ -10,4 +10,4 @@ resource "random_string" "jqnfy" { length = 10 }
 resource "random_string" "ubiim" { length = 10 }
 resource "random_string" "tpbmf" { length = 10 }
 resource "random_string" "xcmxd" { length = 10 }
-resource "random_string" "oflwq" { length = 10 }
+resource "random_string" "yhvnk" { length = 10 }
